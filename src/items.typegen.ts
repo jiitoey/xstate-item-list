64e50d189f4c7aa3edc35a65874e2f58392319e6
jiitoey <68223072+jiitoey@users.noreply.github.com>
2022-03-07 21:04:15 +0700

    init

---
+++ src/items.typegen.ts
@@ -0,0 +1,39 @@
+// This file was automatically generated. Edits will be overwritten
+
+export interface Typegen0 {
+  "@@xstate/typegen": true;
+  eventsCausingActions: {
+    updateItems: "done.invoke.fetch-items";
+    updatePageSize: "PAGE.SIZE_CHANGED";
+    updatePage: "PAGE.PAGE_CHANGED";
+    updateSortBy: "ITEMS.SORT_CHANGED";
+  };
+  internalEvents: {
+    "done.invoke.fetch-items": {
+      type: "done.invoke.fetch-items";
+      data: unknown;
+      __tip: "See the XState TS docs to learn how to strongly type this.";
+    };
+    "xstate.init": { type: "xstate.init" };
+    "error.platform.fetch-items": {
+      type: "error.platform.fetch-items";
+      data: unknown;
+    };
+  };
+  invokeSrcNameMap: {
+    fetchItems: "done.invoke.fetch-items";
+  };
+  missingImplementations: {
+    actions: never;
+    services: never;
+    guards: never;
+    delays: never;
+  };
+  eventsCausingServices: {
+    fetchItems: "ITEMS.SORT_CHANGED" | "ITEMS.RELOAD";
+  };
+  eventsCausingGuards: {};
+  eventsCausingDelays: {};
+  matchesStates: "loading" | "display" | "failed";
+  tags: never;
+}
---
+++ src/tests/items.test.ts
@@ -0,0 +1,101 @@
+import { interpret } from "xstate";
+import { itemsMachine } from "../items";
+
+describe('"fetchItems" on "loading" state', () => {
+  it('"onDone", do "updateItems" and  go to "display"', (done) => {
+    const expectedItems = [
+      {
+        artistName: "John Doe",
+        id: "01234",
+        price: 1.0,
+        currency: "ETH",
+        end: new Date(),
+      },
+    ];
+    const mockFetchMachine = itemsMachine.withConfig({
+      services: {
+        fetchItems: async (_, event) => expectedItems,
+      },
+    });
+    interpret(mockFetchMachine)
+      .onTransition((state) => {
+        if (state.matches("display")) {
+          try {
+            expect(state.context.items).toBe(expectedItems);
+            done();
+          } catch (e) {
+            done(e);
+          }
+        }
+      })
+      .start();
+  });
+
+  it('"onError", go to "failed"', (done) => {
+    const mockFetchMachine = itemsMachine.withConfig({
+      services: {
+        fetchItems: async (_, event) => {
+          throw "This is a forced error!";
+        },
+      },
+    });
+    interpret(mockFetchMachine)
+      .onTransition((state) => {
+        if (state.matches("failed")) {
+          try {
+            expect(state.context.items.length).toBe(0);
+            done();
+          } catch (e) {
+            done(e);
+          }
+        }
+      })
+      .start();
+  });
+});
+
+it('should reach "loading" given "display" when the "ITEMS.SORT_CHANGED" event occurs', () => {
+  const expectedValue = "loading";
+
+  const actualState = itemsMachine.transition("display", {
+    type: "ITEMS.SORT_CHANGED",
+    sortBy: "id",
+  });
+
+  expect(actualState.matches(expectedValue)).toBeTruthy();
+  expect(actualState.context.sortBy).toBe("id");
+});
+
+it('should stay at "display" given "display" when the "PAGE.SIZE_CHANGED" event occurs', () => {
+  const expectedValue = "display";
+
+  const actualState = itemsMachine.transition("display", {
+    type: "PAGE.SIZE_CHANGED",
+    pageSize: 15,
+  });
+
+  expect(actualState.matches(expectedValue)).toBeTruthy();
+  expect(actualState.context.pageSize).toBe(15);
+});
+
+it('should stay at "display" given "display" when the "PAGE.PAGE_CHANGED" event occurs', () => {
+  const expectedValue = "display";
+
+  const actualState = itemsMachine.transition("display", {
+    type: "PAGE.PAGE_CHANGED",
+    page: 2,
+  });
+
+  expect(actualState.matches(expectedValue)).toBeTruthy();
+  expect(actualState.context.page).toBe(2);
+});
+
+it('should reach "loading" given "failed" when the "ITEMS.RELOAD" event occurs', () => {
+  const expectedValue = "loading";
+
+  const actualState = itemsMachine.transition("failed", {
+    type: "ITEMS.RELOAD",
+  });
+
+  expect(actualState.matches(expectedValue)).toBeTruthy();
+});
